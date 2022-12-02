f87e2e2b12683fa55ec4c04e55bc83175c345e37
Roel van Duijnhoven <roel@vanduijnhoven.com>
2022-12-02 22:29:41 +0100

    Solution part 2 of day 1.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -18,21 +18,39 @@ fn sum_of(baggage: &Baggage) -> usize {
 }
 
 fn main() {
+    // Parse input
     let contents = fs::read_to_string("input/part-1.input").unwrap();
-
     let baggages = contents
         .split("\n\n")
         .map(|input| create_from_lines(input))
         .collect::<Vec<Baggage>>();
 
-    let mut highest: Option<Baggage> = None;
-    for baggage in baggages {        
-        highest = match highest {
-            None => Some(baggage),
-            Some(highest_value) if sum_of(&baggage) > sum_of(&highest_value) => Some(baggage),
-            Some(x) => Some(x),
-        };
+    {
+        // Part 1:
+        let mut highest: Option<usize> = None;
+        for baggage in &baggages {        
+            let sum_of_calories = sum_of(baggage);
+            highest = match highest {
+                None => Some(sum_of_calories),
+                Some(highest_value) if sum_of_calories > highest_value => Some(sum_of_calories),
+                Some(x) => Some(x),
+            };
+        }
+
+        println!("The top elf carries {:?} of calories.", highest.unwrap());
     }
 
-    print!("Top result has sum {:?}", sum_of(&highest.unwrap()));
+    {
+        // Part 2:
+        let mut top = baggages
+            .iter()
+            .map(|baggage| sum_of(&baggage))
+            .collect::<Vec<usize>>();
+            
+        top.sort_by(|a, b| b.partial_cmp(a).unwrap());
+
+        let sum_of_top_3: usize = top.iter().take(3).sum();
+
+        println!("The top 3 elfs together carry {:?} of calories with them.", sum_of_top_3);
+    }   
 }
