0e06d699772bd9d4a9f9f4b27c099ec558b8b58e
Roel van Duijnhoven <roel@vanduijnhoven.com>
2022-12-03 22:43:31 +0100

    Solve part 2 of day 3.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,36 +5,76 @@ struct Rucksack {
     back: Vec<char>,
 }
 
+fn rucksack_includes(rucksack: &Rucksack, search: &char) -> bool {
+    for item in all_rucksack_items(&rucksack) {
+        if item == search {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn all_rucksack_items(rucksack: &Rucksack) -> impl Iterator<Item = &char> {
+    rucksack.front.iter().chain(rucksack.back.iter())
+}
+
+fn get_priority_of_item(item: &char) -> u32 {
+    let char = item.clone() as u32;
+    let char_a = 'a' as u32;
+    let char_capitcal_a = 'A' as u32;
+
+    if char >= char_a {
+        return char - char_a + 1;
+    } else {
+        return char - char_capitcal_a + 27;
+    }
+}
+
 fn main() {
     let contents = fs::read_to_string("input/part-1.input").unwrap();
     let lines = contents.split("\n");
 
-    let rucksacks = lines
-        .map(|line| create_rucksack_from_string(line));
+    let rucksacks: Vec<Rucksack> = lines
+        .map(|line| create_rucksack_from_string(line))
+        .collect();
 
+    // Part 1
     let sum: u32 = rucksacks
-        .map(|rucksack: Rucksack| {
-            for char in rucksack.front {
+        .iter()
+        .map(|rucksack: &Rucksack| {
+            for char in &rucksack.front {
                 if rucksack.back.contains(&char) {
                     return char;
                 }
             }
             panic!("Could not parse");
         })
-        .map(|char| {
-            let char = char as u32;
-            let char_a = 'a' as u32;
-            let char_capitcal_a = 'A' as u32;
-
-            if char >= char_a {
-                return char - char_a + 1;
-            } else {
-                return char - char_capitcal_a + 27;
-            }
-        })
+        .map(|item| get_priority_of_item(item))
         .sum();
 
-    println!("{:?}", sum);
+    println!("The sum of the priorities that are packed unnecessary is {:?}.", sum);
+
+    // Part 2
+    let sum: u32 = rucksacks
+        .chunks(3)
+        .map(|chunk| {
+            let a = &chunk[0];
+            let b = &chunk[1];
+            let c = &chunk[2];
+
+            for item in all_rucksack_items(&a) {
+                if rucksack_includes(b, &item) && rucksack_includes(c, &item) {
+                    return item;
+                }
+            }
+
+            panic!("Could not find!");
+        })
+        .map(|item| get_priority_of_item(&item))
+        .sum();
+
+    println!("The sum of the priorities that represent the groups is {:?}.", sum);
 }
 
 fn create_rucksack_from_string(input: &str) -> Rucksack {
