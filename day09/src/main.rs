7f89524685cbeef6205e17e5393b84fafd423de0
Roel van Duijnhoven <roel@vanduijnhoven.com>
2022-12-12 22:18:14 +0100

    Make use of mutual arguments.

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -14,6 +14,11 @@ enum Direction {
     Left,
 }
 
+struct Point {
+    x: isize, 
+    y: isize,
+}
+
 fn read_instructions_from_string(input: &str) -> Vec<Instruction> {
     return input
         .split("\n")
@@ -33,35 +38,30 @@ fn read_instructions_from_string(input: &str) -> Vec<Instruction> {
         .collect();
 }
 
-fn move_head(x: isize, y: isize, direction: &Direction) -> (isize, isize) {
+fn move_head(p: &mut Point, direction: &Direction) {
     match direction {
-        Direction::Up => (x, y + 1),
-        Direction::Right => (x + 1, y),
-        Direction::Down => (x, y - 1),
-        Direction::Left => (x - 1, y),
-    }
+        Direction::Up => p.y += 1,
+        Direction::Right => p.x += 1,
+        Direction::Down => p.y -= 1,
+        Direction::Left => p.x -= 1,
+    };
 }
 
-fn move_tail(tail_x: isize, tail_y: isize, head_x: isize, head_y: isize) -> (isize, isize) {
-    let mut new_tail_x = tail_x;
-    let mut new_tail_y = tail_y;
-    
-    let x_diff = head_x - tail_x;
-    let y_diff = head_y - tail_y;
+fn move_tail(tail: &mut Point, head: &Point) {
+    let x_diff = head.x - tail.x;
+    let y_diff = head.y - tail.y;
     
     let x_step = if x_diff <= -1 { -1 } else if x_diff >= 1 { 1 } else { 0 };
     let y_step = if y_diff <= -1 { -1 } else if y_diff >= 1 { 1 } else { 0 };
 
     if x_diff.abs() > 1 && y_diff == 0 {
-        new_tail_x += x_step;
+        tail.x += x_step;
     } else if y_diff.abs() > 1 && x_diff == 0 {
-        new_tail_y += y_step;
+        tail.y += y_step;
     } else if x_diff.abs() > 1 || y_diff.abs() > 1 {
-        new_tail_x += x_step;
-        new_tail_y += y_step;
+        tail.x += x_step;
+        tail.y += y_step;
     }
-
-    (new_tail_x, new_tail_y)
 }
 
 fn main() {    
@@ -69,20 +69,16 @@ fn main() {
     let instructions = read_instructions_from_string(&input);
 
     // Part 1
-    let mut head_x = 0;
-    let mut head_y = 0;
-
-    let mut tail_x= 0;
-    let mut tail_y = 0;
+    let mut head = Point { x: 0, y: 0 };
+    let mut tail= Point { x: 0, y: 0 };
 
     let mut seen: HashSet<(isize, isize)> = HashSet::new();
-    seen.insert((tail_x, tail_y));
     for instruction in instructions.iter() {
         for _ in 0..instruction.amount {
-            (head_x, head_y) = move_head(head_x, head_y, &instruction.direction);
-            (tail_x, tail_y) = move_tail(tail_x, tail_y, head_x, head_y);
+            move_head(&mut head, &instruction.direction);
+            move_tail(&mut tail, &head);
             
-            seen.insert((tail_x, tail_y));
+            seen.insert((tail.x, tail.y));
         }
     }
 
@@ -90,35 +86,28 @@ fn main() {
 
     // Part 2
     let chain_length = 10;
-    let mut chain: Vec<(isize, isize)> = vec![];
+    let mut chain: Vec<Point> = vec![];
     for _ in 0..chain_length {
-        chain.push((0, 0));
+        chain.push(Point { x: 0, y: 0 });
     }
 
     let mut seen: HashSet<(isize, isize)> = HashSet::new();
-    seen.insert((chain[chain_length - 1].0, chain[chain_length - 1].1));
-
     for instruction in instructions.iter() {
         for _ in 0..instruction.amount {
             // Apply move to first in chain
             let mut head = chain.get_mut(0).unwrap();
-
-            let new_head = move_head(head.0, head.1, &instruction.direction);
-            head.0 = new_head.0;
-            head.1 = new_head.1;            
+            move_head(&mut head, &instruction.direction);
 
             // Now move rest of tail
             for i in 1..chain_length {
-                let (head_x, head_y) = chain[(i - 1) as usize];
+                let head = &chain[(i - 1) as usize];
+                let head = Point { x: head.x, y: head.y }; // TODO Necessary to let this compile.
                 let mut tail = chain.get_mut(i).unwrap();
-
-                let (new_tail_x, new_tail_y) = move_tail(tail.0, tail.1, head_x, head_y);
-                tail.0 = new_tail_x;
-                tail.1 = new_tail_y;
+                move_tail(&mut tail, &head);
             }
 
             // Keep track of last item of chain
-            seen.insert((chain[chain_length - 1].0, chain[chain_length - 1].1));            
+            seen.insert((chain[chain_length - 1].x, chain[chain_length - 1].y));            
         }
     }
 
