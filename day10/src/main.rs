75470003b8a0724e428bb87fb44852b0dfe5ee1b
Roel van Duijnhoven <roel@vanduijnhoven.com>
2022-12-13 08:27:36 +0100

    Part 2 of day 10.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,7 +1,7 @@
 use core::panic;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Instruction {
     Noop,
     AddX(isize),
@@ -31,6 +31,17 @@ fn read_program_from_string(input: &str) -> Vec<Instruction> {
             }
         })
         .collect();
+}
+
+fn create_program_from_instructions(instructions: &Vec<Instruction>) -> Program {
+    return Program { 
+        instructions: instructions.clone(), 
+        program_counter: 0, 
+        cycle_counter: 0, 
+        value_x: 1, 
+        cycles_waiting: 0, 
+        add_when_finished: 0,
+     };
 }
 
 fn run_cycle(program: &mut Program) {
@@ -66,7 +77,8 @@ fn main() {
     let input = fs::read_to_string("assets/problem.input").unwrap();
     let instructions = read_program_from_string(&input);
 
-    let mut program = Program { instructions, program_counter: 0, cycle_counter: 0, value_x: 1, cycles_waiting: 0, add_when_finished: 0 };
+    // Part 1
+    let mut program = create_program_from_instructions(&instructions);
 
     let mut sum_of_signals = 0;
     for cycles in [20, 40, 40, 40, 40, 40] {
@@ -75,4 +87,20 @@ fn main() {
     }
 
     println!("Sum of signal strenghts is {}", sum_of_signals);
+
+    // Part 2
+    let mut program = create_program_from_instructions(&instructions);
+
+    for _ in 0..6 {
+        for column in 0..40 {
+            run_cycle(&mut program);
+
+            if column >= program.value_x - 1 && column <= program.value_x + 1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }    
 }
