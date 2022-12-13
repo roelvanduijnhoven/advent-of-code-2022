27a8000d0cb602daa463123c0e5bf88435740347
Roel van Duijnhoven <roel@vanduijnhoven.com>
2022-12-13 22:48:49 +0100

    Part 2 of day 11.

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -34,12 +34,29 @@ fn create_monkey_from(block: &str) -> Monkey {
      }
 }
 
+fn create_monkeys_from(input: &str) -> Vec<Monkey> {
+    return input
+        .split("\n\n")
+        .map(|block| create_monkey_from(&block))
+        .collect();
+}
+
 fn pop_item_from_monkey(monkey: &mut Monkey) -> isize {
     monkey.inspected_items += 1;
     return monkey.items.remove(0);
 }
 
+// TODO Hardcoded. Could obviously be improved.
 fn do_monkey_operation(input: isize, monkey: &Monkey) -> isize {
+    // The Hardcoded part of the example problem.
+    // return match monkey.id {
+    //     0 => input * 19,
+    //     1 => input + 6,
+    //     2 => input * input,
+    //     3 => input + 3,
+    //     _ => panic!("Unhandled monkey operations"),
+    // };
+
     match monkey.id {
         0 => input * 13,
         1 => input + 2,
@@ -53,12 +70,24 @@ fn do_monkey_operation(input: isize, monkey: &Monkey) -> isize {
     }
 }
 
-fn monkey_turn(monkeys: &mut Vec<Monkey>, monkey_id: usize) {
+fn monkey_turn(monkeys: &mut Vec<Monkey>, monkey_id: usize, divide_by_three: bool) {
+    let gcd: isize = monkeys
+        .iter()
+        .map(|monkey| monkey.divisable_test as isize)
+        .product();
+
     while monkeys[monkey_id].items.len() > 0 {
         let current_item = pop_item_from_monkey(monkeys.get_mut(monkey_id).unwrap());
 
         let monkey = monkeys.get(monkey_id).unwrap();
-        let new_item = do_monkey_operation(current_item, &monkey).div(3);
+        let mut new_item = do_monkey_operation(current_item, &monkey);
+
+        if divide_by_three {
+            new_item = new_item.div(3);
+        }
+
+        // Make sure the integers don't grow out of bounds.
+        new_item = new_item % gcd;
 
         let is_divisible = new_item.rem(monkey.divisable_test as isize) == 0;
 
@@ -68,33 +97,41 @@ fn monkey_turn(monkeys: &mut Vec<Monkey>, monkey_id: usize) {
             monkey.false_branch
         };
 
-        // println!("Item with worry {} is thrown from monkey {} to to monkey {}", new_item, monkey_id, monkey_recipient);
-
         monkeys.get_mut(monkey_recipient).unwrap().items.push(new_item);
     }
 }
 
-fn do_monkey_round(monkeys: &mut Vec<Monkey>) {
+fn do_monkey_round(monkeys: &mut Vec<Monkey>, divide_by_three: bool) {
     for i in 0..monkeys.len() {
-        monkey_turn(monkeys, i);
+        monkey_turn(monkeys, i, divide_by_three);
     }
+}
+
+fn get_monkey_score(monkeys: &Vec<Monkey>) -> usize {
+    let mut result: Vec<usize> = monkeys
+        .iter()
+        .map(|monkey| monkey.inspected_items)
+        .collect();
+    result.sort_by(|a, b| b.partial_cmp(&a).unwrap());
+    result[0] * result[1]
 }
 
 fn main() {
     let input = fs::read_to_string("assets/problem.input").unwrap();
 
-    let mut monkeys: Vec<Monkey> = input
-        .split("\n\n")
-        .map(|block| create_monkey_from(&block))
-        .collect();
-
+    // Part 1
+    let mut monkeys = create_monkeys_from(&input);
     for _ in 0..20 {
-        do_monkey_round(&mut monkeys);
+        do_monkey_round(&mut monkeys, true);
     }
 
-    monkeys.sort_by(|a, b| b.inspected_items.partial_cmp(&a.inspected_items).unwrap() );
+    println!("After 20 rounds we have a monkey score of {}", get_monkey_score(&monkeys));
+    
+    // Part 2
+    let mut monkeys = create_monkeys_from(&input);
+    for _ in 0..10_000 {
+        do_monkey_round(&mut monkeys, false);
+    }
 
-    let solution = monkeys[0].inspected_items * monkeys[1].inspected_items;
-
-    println!("{:?}", solution);
+    println!("After 10.000 rounds we have a monkey score of {}", get_monkey_score(&monkeys));
 }
